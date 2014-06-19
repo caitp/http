3fcc53cad376bd03242c12fe8c02620fc18414b6
Caitlin Potter <caitpotter88@gmail.com>
2014-06-19 14:37:02 -0400

    chore(karma.conf.js): slight adjustments to files loaded and preprocessed
    These adjustments match working patterns from watchtower.js and other v2 repos, and are
    working in my local tests.

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
       {pattern: 'src/**/*.js', included: false},
       {pattern: 'test/**/*.js', included: false},
-      {pattern: 'node_modules/rtts-assert/dist/amd/**/*.js', included: false},
+      {pattern: 'node_modules/rtts-assert/src/**/*.js', included: false},
       {pattern: 'node_modules/di/src/*.js', included: false},
       {pattern: 'node_modules/di/node_modules/es6-shim/es6-shim.js', included: false},
       {pattern: 'node_modules/di/node_modules/q/q.js', included: false},
@@ -21,6 +21,7 @@ module.exports = function(config) {
     preprocessors: {
       'node_modules/deferred/src/*.js': ['traceur'],
       'node_modules/di/src/*.js': ['traceur'],
+      'node_modules/rtts-assert/src/**/*.js': ['traceur'],
       'src/**/*.js': ['traceur'],
       'test/**/*.js': ['traceur'],
     }
--- test-main.js
+++ test-main.js
@@ -1,31 +1,33 @@
-var allTestFiles = [];
-var TEST_REGEXP = /\.spec\.js$/;
+(function() {
+  var allTestFiles = [];
+  var TEST_REGEXP = /\.spec\.js$/;
 
-var pathToModule = function(path) {
-  return path.replace(/^\/base\//, '').replace(/\.js$/, '');
-};
+  var pathToModule = function(path) {
+    return path.replace(/^\/base\//, '').replace(/\.js$/, '');
+  };
 
-Object.keys(window.__karma__.files).forEach(function(file) {
-  if (TEST_REGEXP.test(file)) {
-    // Normalize paths to RequireJS module names.
-    allTestFiles.push(pathToModule(file));
-  }
-});
+  Object.keys(window.__karma__.files).forEach(function(file) {
+    if (TEST_REGEXP.test(file)) {
+      // Normalize paths to RequireJS module names.
+      allTestFiles.push(pathToModule(file));
+    }
+  });
 
-require.config({
-  // Karma serves files under /base, which is the basePath from your config file
-  baseUrl: '/base',
+  require.config({
+    // Karma serves files under /base, which is the basePath from your config file
+    baseUrl: '/base',
 
-  paths: {
-    'q': './node_modules/di/node_modules/q/q',
-    'di': './node_modules/di/src',
-    'assert': './node_modules/rtts-assert/dist/amd/assert',
-    'deferred': './node_modules/deferred/src'
-  },
+    paths: {
+      'q': './node_modules/di/node_modules/q/q',
+      'di': './node_modules/di/src',
+      'assert': './node_modules/rtts-assert/src/assert',
+      'deferred': './node_modules/deferred/src'
+    },
 
-  // Dynamically load all test files and ES6 polyfill.
-  deps: allTestFiles.concat(['node_modules/di/node_modules/es6-shim/es6-shim', 'test/matchers']),
+    // Dynamically load all test files and ES6 polyfill.
+    deps: allTestFiles.concat(['node_modules/di/node_modules/es6-shim/es6-shim', 'test/matchers']),
 
-  // we have to kickoff jasmine, as it is asynchronous
-  callback: window.__karma__.start
-});
+    // we have to kickoff jasmine, as it is asynchronous
+    callback: window.__karma__.start
+  });
+})();
