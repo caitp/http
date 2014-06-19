1951a38c933876223145db865cce33e04dbd5fc6
Jeff Cross <middlefloor@gmail.com>
2014-06-19 15:22:29 -0700

    fix(Serialize): add support for all valid JSON values

--- src/Serialize.js
+++ src/Serialize.js
@@ -2,8 +2,11 @@ function serialize (data) {
   if (data instanceof Object) {
     return JSON.stringify(data);
   }
+  else if (typeof data === 'undefined') {
+    return '';
+  }
   else {
-    return data;
+    return `${data}`;
   }
 }
 
